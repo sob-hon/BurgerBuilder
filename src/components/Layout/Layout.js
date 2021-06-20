ac13c40b16dfc3fe68072aa6d1923e4707cc7e88
Sobhan Yazdanjoo <sobhanyazdanjo@gmail.com>
2021-06-20 16:23:04 +0430

    Add hamburger menu, side drawer and make functionality for all of 'em.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import Auxiliary from '../../hoc/Auxiliary';
+import React, { Component } from "react";
+import Auxiliary from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
-import Toolbar from "../Navigation/Toolbar/Toolbar"
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
 
-const Layout = ({children}) => {
-  return(
-    <Auxiliary>
-      <Toolbar />
-      <div>Toolbar, SideDrawer, Backdrop</div>
-      <main className={classes.Content}>{children}</main>
-    </Auxiliary>
-  )
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false,
+    });
+  };
+
+  drawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {
+        showSideDrawer: !prevState.showsideDrawer,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <Auxiliary>
+        <Toolbar drawerToggleClicked={this.drawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>{this.props.children}</main>
+      </Auxiliary>
+    );
+  }
 }
 
 export default Layout;
